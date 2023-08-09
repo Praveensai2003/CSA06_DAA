09096d9cf88b6beba72b8b49089cbabd5f48cf75
Praveensai2003 <112741111+Praveensai2003@users.noreply.github.com>
2023-08-09 13:05:05 +0530

    Update Mul matrix.c

--- Mul matrix.c
+++ Mul matrix.c
@@ -1,59 +1,82 @@
 #include <stdio.h>
 
-void multiplyMatrices(int mat1[][100], int mat2[][100], int result[][100], int rows1, int cols1, int rows2, int cols2) {
-    for (int i = 0; i < rows1; i++) {
-        for (int j = 0; j < cols2; j++) {
-            result[i][j] = 0;
-            for (int k = 0; k < cols1; k++) {
-                result[i][j] += mat1[i][k] * mat2[k][j];
-            }
-        }
-    }
+// function to get matrix elements entered by the user
+void getMatrixElements(int matrix[][10], int row, int column) {
+
+   printf("\nEnter elements: \n");
+
+   for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < column; ++j) {
+         printf("Enter a%d%d: ", i + 1, j + 1);
+         scanf("%d", &matrix[i][j]);
+      }
+   }
 }
 
-void displayMatrix(int mat[][100], int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d ", mat[i][j]);
-        }
-        printf("\n");
-    }
+// function to multiply two matrices
+void multiplyMatrices(int first[][10],
+                      int second[][10],
+                      int result[][10],
+                      int r1, int c1, int r2, int c2) {
+
+   // Initializing elements of matrix mult to 0.
+   for (int i = 0; i < r1; ++i) {
+      for (int j = 0; j < c2; ++j) {
+         result[i][j] = 0;
+      }
+   }
+
+   // Multiplying first and second matrices and storing it in result
+   for (int i = 0; i < r1; ++i) {
+      for (int j = 0; j < c2; ++j) {
+         for (int k = 0; k < c1; ++k) {
+            result[i][j] += first[i][k] * second[k][j];
+         }
+      }
+   }
+}
+
+// function to display the matrix
+void display(int result[][10], int row, int column) {
+
+   printf("\nOutput Matrix:\n");
+   for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < column; ++j) {
+         printf("%d  ", result[i][j]);
+         if (j == column - 1)
+            printf("\n");
+      }
+   }
 }
 
 int main() {
-    int rows1, cols1, rows2, cols2;
+   int first[10][10], second[10][10], result[10][10], r1, c1, r2, c2;
+   printf("Enter rows and column for the first matrix: ");
+   scanf("%d %d", &r1, &c1);
+   printf("Enter rows and column for the second matrix: ");
+   scanf("%d %d", &r2, &c2);
 
-    printf("Enter the number of rows and columns for the first matrix: ");
-    scanf("%d %d", &rows1, &cols1);
+   // Taking input until
+   // 1st matrix columns is not equal to 2nd matrix row
+   while (c1 != r2) {
+      printf("Error! Enter rows and columns again.\n");
+      printf("Enter rows and columns for the first matrix: ");
+      scanf("%d%d", &r1, &c1);
+      printf("Enter rows and columns for the second matrix: ");
+      scanf("%d%d", &r2, &c2);
+   }
 
-    printf("Enter the number of rows and columns for the second matrix: ");
-    scanf("%d %d", &rows2, &cols2);
+   // get elements of the first matrix
+   getMatrixElements(first, r1, c1);
 
-    if (cols1 != rows2) {
-        printf("Matrix multiplication is not possible.\n");
-        return 0;
-    }
+   // get elements of the second matrix
+   getMatrixElements(second, r2, c2);
 
-    int mat1[100][100], mat2[100][100], result[100][100];
+   // multiply two matrices.
+   multiplyMatrices(first, second, result, r1, c1, r2, c2);
 
-    printf("Enter the elements of the first matrix:\n");
-    for (int i = 0; i < rows1; i++) {
-        for (int j = 0; j < cols1; j++) {
-            scanf("%d", &mat1[i][j]);
-        }
-    }
+   // display the result
+   display(result, r1, c2);
 
-    printf("Enter the elements of the second matrix:\n");
-    for (int i = 0; i < rows2; i++) {
-        for (int j = 0; j < cols2; j++) {
-            scanf("%d", &mat2[i][j]);
-        }
-    }
-
-    multiplyMatrices(mat1, mat2, result, rows1, cols1, rows2, cols2);
-
-    printf("Resultant matrix after multiplication:\n");
-    displayMatrix(result, rows1, cols2);
-
-    return 0;
+   return 0;
 }
